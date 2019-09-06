c64f29bd3cf32067909415ccca0868a993e7b89f
Marc Sanchez <marcsanchez@me.com>
2019-09-05 21:28:14 -0400

    all files to date

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// This is to hide and show the Our Story text
+const readMoreButton = document.querySelector('button.read-more');
+const readMoreText = document.querySelector('.read-more-text');
+
+$('.read-more-text').hide();
+
+readMoreButton.addEventListener('click', () => {
+  if (readMoreText.style.display == 'none') {
+    readMoreButton.textContent = 'Show Less';
+    readMoreText.style.display = 'block';
+  } else {
+    readMoreButton.textContent = 'Show More';
+    readMoreText.style.display = 'none';
+    }
+});
+
+// This is for the RSVP
+function addMessage() {
+  var msg = 'Welcome to';
+  var el = document.getElementById('message');
+  el.textContent = msg;
+}
+
+addMessage();
+
+function callMessage() {
+  el.textContent = newMessage;
+  return addMessage();
+}
+
+$(".footer_powered_image").hide();
